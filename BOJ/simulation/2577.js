67167f6a8d0a810e8710350396fd9aa269718629
gygy7151 <gygy7151@gmail.com>
2022-11-15 14:03:40 +0900

    feat: solved 2577 of BOJ in stack

---
+++ BOJ/simulation/2577.js
@@ -0,0 +1,18 @@
+// 숫자의 개수
+// 두번째 풀이
+let [...input] = require("fs")
+	.readFileSync("/dev/stdin")
+	.toString()
+	.trim()
+	.split("\n")
+	.map(num => parseInt(num));
+
+let countDigit = new Array(10).fill(0);
+const result = input
+	.reduce((prev, next) => prev * next)
+	.toString()
+	.split("");
+
+let answer = "";
+result.forEach(digit => countDigit[digit]++); // 아 js는 str이든 int든 배열에선 타입 상관없음
+countDigit.forEach(x => console.log(x));
