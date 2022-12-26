7f97764b488dba53cbc0325ff53c874fc0b9ba9b
gygy7151 <gygy7151@gmail.com>
2022-12-26 23:59:18 +0900

    conf: Create 11729.js of BOJ

---
+++ BOJ/simulation/11729.js
@@ -0,0 +1,33 @@
+// 하노이의 탑 이동
+// 첫번째 풀이
+// [ 접근방법 ]
+// 처음엔 단순 그리드 구현인줄 알았는데, 재귀로 접근해야 했다.
+// 참조: https://nyang-in.tistory.com/210
+let fs = require('fs');
+let input = fs.readFileSync("/dev/stdin").toString();
+
+let N = Number(input); // 원판의 갯수
+let count = 0;
+let answer = [];
+
+// num : 원반의 개수
+// from : 출발지 기둥 번호
+// to : 목적지 기둥 번호
+// other : 나머지 기둥 번호
+
+function Hanoi(num, from, other, to){
+    if(num === 0) {
+        return;
+    }else{
+        // 받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 곳으로 재귀적으로 이동
+        Hanoi(num - 1 , from, to, other);
+        // 맨 아래 원반을 목적지로 이동시킴
+        answer.push([from, to]);
+        count++;
+        //다른 곳으로 옮겼던 원반들을 그 위에 얹음
+        Hanoi(num - 1, other, from, to);
+    }
+}
+Hanoi(N, '1','2','3');
+console.log(count);
+console.log(answer.map((element) => element.join(" ")).join("\n"));
